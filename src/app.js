265ab1e3d0251a21106717867d67b336cd03abc9
JoelAlexanderTrinidad <joelalexandertrinidad@gmail.com>
2022-06-18 13:51:12 -0300

    sequelize txt

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 
@@ -22,4 +24,4 @@ app.use('/', indexRouter);
 app.use(moviesRoutes);
 app.use(genresRoutes);
 
-app.listen('3001', () => console.log('Servidor corriendo en el puerto 3001'));
+app.listen(process.env.PORT, () => console.log(`Servidor corriendo en http://localhost:${process.env.PORT}`));
